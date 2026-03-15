1534ac9e84f547c3780d431f234c7dc053f034e0
Dhouglas Bandeira <62654697+dhouglasbn@users.noreply.github.com>
2026-03-15 15:30:43 -0300

    Bind server to all network interfaces

--- src/server.ts
+++ src/server.ts
@@ -53,6 +53,9 @@ app.register(getSubscriberRankingPositionRoute)
 app.register(getRankingRoute)
 app.register(sendMessageRoute)
 
-app.listen({ port: env.PORT }).then(() => {
+app.listen({
+  port: env.PORT,
+  host: '0.0.0.0',
+}).then(() => {
   console.log(`HTTP server running on PORT ${env.PORT}!`)
 })
